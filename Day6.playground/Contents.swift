b4496d0b52f8e18e7607f3179b4a07730f53cb8a
Tommy H <tommyhoangis@gmail.com>
2017-06-17 08:41:51 -0500

    Day 6 - Class Initializer and code challenge

--- Day6.playground/Contents.swift
+++ Day6.playground/Contents.swift
@@ -46,25 +46,3 @@ book.recipes.append(Recipe(aname: "Candy", fullDescription: "sugar, color", rati
 
 book.recipes[2]
 
-//  ****************
-//  CODE CHALLENGE
-//  ****************
-
-/*
- Create a new class called app - remember UpperCamelCasing?
- This class has several properties: name, developer, rating, category, and price.
- Create those properties and determine what their data types are.
- Create an initializer for the class that takes all the required information like name, developer, rating, category and price. Initialize these values to our properties.
- 
- Now let's create two new instances from your new App class to test it out and use in the upcoming steps.
- 
- An app store has tons of apps so let's create a class called AppStore.
- This class has only one property which is an array of App.
- Create its initializer which takes an array of apps and initialize the value to our property.
- Now, let's create a constant called sampleApps and initialize it by constructing a new array with all the elements being the two App instances you have in step 6.
- 
- Create a new instance of AppStore name it appleAppStore, pass in the sampleApps above.
- Can you try to access the second app in all the apps of the appleAppStore?
- Can you try to access developer property of the first app of all the apps in appleAppStore?
- */
-
